d32542dc35c136e14cbb333dde9fcd691e013bf6
miguelazue <miguelazue@gmail.com>
2024-10-14 16:28:49 -0500

    Update create_tables.sql

--- bank-warehouse/create_tables.sql
+++ bank-warehouse/create_tables.sql
@@ -43,6 +43,7 @@ CREATE TABLE location (
 );
 
 
+
 CREATE TABLE transfers (
     id BIGINT PRIMARY KEY,
     account_id BIGINT,
@@ -52,4 +53,34 @@ CREATE TABLE transfers (
     transaction_requested_at TIMESTAMP,
     status VARCHAR(128)
 );
+
+-- Partition by Year
+/*
+CREATE TABLE transfers (
+    id BIGINT PRIMARY KEY,
+    account_id BIGINT,
+    transaction_type VARCHAR(128),
+    amount FLOAT,
+    transaction_requested_date DATE,
+    transaction_requested_at TIMESTAMP,
+    status VARCHAR(128)
+)
+PARTITION BY RANGE (YEAR(transaction_requested_date)) (
+    PARTITION p2020 VALUES LESS THAN (2021),
+    PARTITION p2021 VALUES LESS THAN (2022),
+    PARTITION p2022 VALUES LESS THAN (2023),
+    PARTITION p2023 VALUES LESS THAN (2024),
+    PARTITION p2025 VALUES LESS THAN (2025),
+    PARTITION p_max VALUES LESS THAN MAXVALUE  -- For future dates
+);
+*/
+
+-- Indexing by IDs frequently used in joins
+
+CREATE UNIQUE INDEX idx_customer_id ON customers (customer_id);
+CREATE UNIQUE INDEX idx_account_id ON accounts (account_id);
+
+-- Create index on orders table
+CREATE INDEX idx_customer_id ON accounts (customer_id);
+CREATE INDEX idx_account_id ON transfers (account_id);
 
